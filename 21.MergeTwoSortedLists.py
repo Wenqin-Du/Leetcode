6edeedcf22b9c1978baee7d0321d9c6fa72c6239
Wenqin Du <32427262+Wenqin-Du@users.noreply.github.com>
2020-07-25 14:28:45 -0600

    Add files via upload

--- 21.MergeTwoSortedLists.py
+++ 21.MergeTwoSortedLists.py
@@ -7,6 +7,7 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
 
+        ### Method 1. while & needle
         curr = dummy = ListNode(0)
 
         while l1 and l2:
@@ -23,3 +24,10 @@ class Solution:
 
         return dummy.next
 
+        ### Method 2. Recursion
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            return ListNode(l1.val, self.mergeTwoLists(l1.next,l2))
+        else:
+            return ListNode(l2.val, self.mergeTwoLists(l1,l2.next))
